c51b656c0988e0143eff7550b14d7246295dd257
taeyoung <xodud5080@naver.com>
2024-02-08 17:03:11 +0900

    3차 수정

--- src/for_/Question.java
+++ src/for_/Question.java
@@ -1,7 +1,11 @@
 package for_;
 
+import java.util.Random;
+
 public class Question {
 	public static void main(String[] args) {
 		System.out.println("2조 4번, 1조 3번, 6조 2번, 6조 3번");
+		Random random = new Random();
+		int result = random.nextInt(2)+1;// 0이상 2미만 / (0, 1) -> (1, 2)
 	}
 }
--- src/for_/Quiz17.java
+++ src/for_/Quiz17.java
@@ -1,5 +1,11 @@
 package for_;
 
-public class Quiz17 {
+import java.util.Random;
 
+public class Quiz17 {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int result = random.nextInt(3)+1;
+		System.out.println(result);
+	}
 }
